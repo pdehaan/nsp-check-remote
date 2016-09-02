e5d14f5614f55801e52e8964abdc69da5b62a391
Peter deHaan <peter@deseloper.com>
2016-09-02 00:55:42 -0700

    Switch to fetch-nice-package (#3)

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const nsp = require('nsp');
-const packageJson = require('package-json');
+const packageJson = require('fetch-nice-package');
+const sortJson = require('sort-json');
+const stripcolorcodes = require('stripcolorcodes');
 
-exports.checkLatest = (name, formatter = 'default') => packageJson(name, 'latest')
-    .then((package) => new Promise((resolve) => {
-      nsp.check({package}, (err, data) => {
-        const fmt = nsp.getFormatter(formatter);
-        const output = fmt(err, data);
-        resolve({err, data, output, name, package});
+// Unwanted keys to delete from package.json.
+const PACKAGE_JSON_KEYS_TO_REMOVE = ['lastPublisher', 'owners', 'readme', 'stars', 'versions'];
+
+function cleanPackage(package) {
+  PACKAGE_JSON_KEYS_TO_REMOVE.forEach((key) => delete package[key]);
+
+  // Return the repo HTTPS URL, if available.
+  if (package.repository) {
+    package.repository = package.repository.https_url;
+  }
+
+  // Set some default values.
+  package.dependencies = package.dependencies || {};
+  package.devDependencies = package.devDependencies || {};
+  package.keywords = package.keywords || [];
+
+  return sortJson(package);
+}
+
+exports.checkLatest = (name, formatter = 'default') => packageJson(name)
+  .then((package) => new Promise((resolve) => {
+    nsp.check({package}, (err, data) => {
+      const fmt = nsp.getFormatter(formatter);
+      const output = fmt(err, data);
+
+      resolve({
+        data,
+        err,
+        name,
+        // Strip any nsp/chalk color codes from the output.
+        output: stripcolorcodes(output),
+        package: cleanPackage(package)
       });
-    }));
+    });
+  }));
